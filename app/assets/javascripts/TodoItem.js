054aa434e9d8b988da2e737c975c993fa5c02d13
Jake Austin <jakeaust07@gmail.com>
2014-07-18 15:12:57 -0400

    include, set, and show timestamps for created at and completed at

--- app/assets/javascripts/TodoItem.js
+++ app/assets/javascripts/TodoItem.js
@@ -1,13 +1,15 @@
 var TodoItem = function(title){
   this.done = false;
   this.title = title;
-  this.createdAt = Date.now();
+  this.createdAt = new Date();
+  this.completedAt = 0; // what for default??
 };
 
 TodoItem.prototype = {
 
     complete: function() {
       this.done = true;
+      this.completedAt = new Date();
     },
 
     //doing nothing
@@ -23,7 +25,7 @@ TodoItem.prototype = {
       deleteButton.attr("data-id", id);
       deleteButton.attr("class", 'delete');
 
-      var newItem = $('<li>').text(this.title);
+      var newItem = $('<li>').text(this.title  + ' -created- ' + this.createdAt);
       newItem.attr("data-id", id);
    //   newItem.attr("data-time", this.createdAt);
       newItem.attr("data-status", this.done);
@@ -37,8 +39,7 @@ TodoItem.prototype = {
       var deleteButton = $('<button>').text('Remove');
       deleteButton.attr("data-id", id);
       deleteButton.attr("class", 'delete');
-
-      var newItem = $('<li>').text(this.title);
+      var newItem = $('<li>').text(this.title + ' -completed- ' + this.completedAt);
       newItem.attr("data-id", id);
    //   newItem.attr("data-time", this.createdAt);
       newItem.attr("data-status", this.done);
