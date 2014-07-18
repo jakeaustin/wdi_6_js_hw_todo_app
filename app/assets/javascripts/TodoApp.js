27116305c5d411c6a845d35cd1a3cb72c850f828
Jake Austin <jakeaust07@gmail.com>
2014-07-17 21:03:04 -0400

    basic layout, todos can be created with what, when, and done

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,3 +1,19 @@
 var TodoApp = {
+  initialize: function() {
+    this.todos = [];
+      $('#todo-form').submit(this.createTodo);
+  },
+  createTodo: function(event) {
+    event.preventDefault();
+    var userIn = $(this).find('input').val();
+    var time = new Date();
+    var completed = false;
 
+    var newTodo = { what: userIn, when: time, done: completed };
+    var newItem = $('<li>').text(newTodo.what);
+    $('#todos-list').append(newItem);
+
+    TodoApp.todos.push(newTodo);
+   }
 };
+
