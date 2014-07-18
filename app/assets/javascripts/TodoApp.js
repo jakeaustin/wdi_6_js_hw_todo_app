ff1339028684bb17e7942b2875bdbb55ceb66bd5
Jake Austin <jakeaust07@gmail.com>
2014-07-17 21:52:29 -0400

    in the console, todos can be completed and added to todones (though not removed from todos)

--- app/assets/javascripts/TodoApp.js
+++ app/assets/javascripts/TodoApp.js
@@ -1,19 +1,40 @@
 var TodoApp = {
   initialize: function() {
     this.todos = [];
+    this.todones = [];
       $('#todo-form').submit(this.createTodo);
   },
   createTodo: function(event) {
     event.preventDefault();
     var userIn = $(this).find('input').val();
     var time = new Date();
-    var completed = false;
+    var finished = false;
 
-    var newTodo = { what: userIn, when: time, done: completed };
+
+    //sets done to true, moves item into todones list
+    //does NOT remove item from todo list
+    //items need coplete & delete link accompany in li
+    var complete = function() {
+      this.done = true;
+      TodoApp.todones.push(this);
+      var newItem = $('<li>').text(this.what);
+      $('#todones-list').append(newItem);
+
+      TodoApp.todos.splice(TodoApp.todos.indexOf(this.what), 1);
+    };
+
+    //doing nothing
+    var _delete = function() {
+      console.log(this);
+    };
+
+    //items need coplete & delete link accompany in li
+    var newTodo = { what: userIn, when: time, done: finished, complete: complete };
     var newItem = $('<li>').text(newTodo.what);
     $('#todos-list').append(newItem);
 
     TodoApp.todos.push(newTodo);
    }
+
 };
 
